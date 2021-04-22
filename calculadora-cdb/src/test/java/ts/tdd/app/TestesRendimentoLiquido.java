f558942add7c69f25bc43d194dd13af944a78b71
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2021-04-22 19:42:01 -0300

    Teste c - Calculo do Rendimento Liquido 3 - Obter barra vermelha por novo teste para a mesma unidade previamente implementada
    Co-authored-by: Kevin Luis <k.luis360@gmail.com

--- calculadora-cdb/src/test/java/ts/tdd/app/TestesRendimentoLiquido.java
+++ calculadora-cdb/src/test/java/ts/tdd/app/TestesRendimentoLiquido.java
@@ -12,5 +12,11 @@ public class TestesRendimentoLiquido
     {
         CDB aplicacao = new CDB(60, 1000.00f, 8.5f);
 	    assertEquals(1.08f, aplicacao.calcularRendimentoLiquido(), 0);
-    }   
+    }
+    @Test
+    public void TesteCalculoRendimentoLiquido2()
+    {
+        CDB aplicacao = new CDB(120, 500f, 8f);
+        assertEquals(2.04f, aplicacao.calcularRendimentoLiquido(), 0);
+    }
 }
