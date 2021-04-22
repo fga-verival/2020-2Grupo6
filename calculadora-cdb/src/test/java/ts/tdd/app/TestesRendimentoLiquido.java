7816e73683d0ce87bd7d0870539c7b82f0359c59
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2021-04-22 20:00:42 -0300

    Teste c - Calculo do Rendimento Liquido 5 - Obter barra verde apos novo teste a partir da tecnica de triangulaçao
    Co-authored-by: Kevin Luis <k.luis360@gmail.com>

--- calculadora-cdb/src/test/java/ts/tdd/app/TestesRendimentoLiquido.java
+++ calculadora-cdb/src/test/java/ts/tdd/app/TestesRendimentoLiquido.java
@@ -13,10 +13,20 @@ public class TestesRendimentoLiquido
         CDB aplicacao = new CDB(60, 1000.00f, 8.5f);
 	    assertEquals(1.08f, aplicacao.calcularRendimentoLiquido(), 0.1f);
     }
+
     @Test
     public void TesteCalculoRendimentoLiquido2()
     {
         CDB aplicacao = new CDB(120, 500f, 8f);
         assertEquals(2.04f, aplicacao.calcularRendimentoLiquido(), 0.1f);
+    }
+    
+    @Test
+    public void TesteCalculoRendimentoBruto3()
+    {
+        CDB aplicacao1 = new CDB(240, 3000f, 9f);
+        CDB aplicacao2 = new CDB(270, 2000f, 8.5f);
+        assertEquals(4.73f, aplicacao1.calcularRendimentoLiquido(), 0.01f);
+        assertEquals(5.03f, aplicacao2.calcularRendimentoLiquido(), 0.01f);
     }
 }
